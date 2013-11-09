233895f32ee37e9899e4a9df21e8611b389e089d
tekuto <g.tekuto@gmail.com>
2013-11-09 17:03:30 +0900

    文字コード変換処理を修正

--- src/sucrose4cpp/common/linux/stringconverter.cpp
+++ src/sucrose4cpp/common/linux/stringconverter.cpp
@@ -5,52 +5,24 @@
 #include "gf4cpp/common/primitives.h"
 
 namespace {
-    gf::Size getMaxStringCharLength(
-    )
-    {
-        return GF_STRING_MAX_CHAR_LENGTH;
-    }
-
-    gf::Size getMaxCharLength(
-        const gf::Utf8 &
-    )
-    {
-        return GF_UTF8_MAX_CHAR_LENGTH;
-    }
-
-    gf::Size getMaxCharLength(
-        const gf::Utf16 &
-    )
-    {
-        return GF_UTF16_MAX_CHAR_LENGTH;
-    }
-
-    gf::Size getMaxCharLength(
-        const gf::Utf32 &
-    )
-    {
-        return GF_UTF32_MAX_CHAR_LENGTH;
-    }
-
     template<
-        typename TO_T
-        , typename FROM_T
+        gf::Size MAX_TO_CHAR_LENGTH
         , typename CONVERTER
+        , typename TO_T
+        , typename FROM_T
     >
     gf::Bool convert(
         TO_T &              _to
         , const FROM_T &    _FROM
-        , const CONVERTER & _CONVERTER
-        , gf::Size          _maxToCharLength
     )
     {
         const auto  FROM_LENGTH = _FROM.length();
         const auto  FROM = _FROM.c_str();
 
-        auto                        bufferLength = FROM_LENGTH * _maxToCharLength;
+        auto                        bufferLength = FROM_LENGTH * MAX_TO_CHAR_LENGTH;
         typename TO_T::value_type   buffer[ bufferLength ];
 
-        if( _CONVERTER(
+        if( CONVERTER()(
             buffer
             , bufferLength
             , FROM
@@ -67,40 +39,275 @@ namespace {
         return true;
     }
 
-    template<
-        typename FROM_T
-        , typename CONVERTER
-    >
+    struct ToString
+    {
+        gf::Bool operator()(
+            gf::StringChar *        _to
+            , gf::Size &            _toLength
+            , const gf::Utf8Char *  _FROM
+            , gf::Size              _fromLength
+        )
+        {
+            return gfStringFromUtf8(
+                _to
+                , &_toLength
+                , _FROM
+                , _fromLength
+            );
+        }
+
+        gf::Bool operator()(
+            gf::StringChar *        _to
+            , gf::Size &            _toLength
+            , const gf::Utf16Char * _FROM
+            , gf::Size              _fromLength
+        )
+        {
+            return gfStringFromUtf16(
+                _to
+                , &_toLength
+                , _FROM
+                , _fromLength
+            );
+        }
+
+        gf::Bool operator()(
+            gf::StringChar *        _to
+            , gf::Size &            _toLength
+            , const gf::Utf32Char * _FROM
+            , gf::Size              _fromLength
+        )
+        {
+            return gfStringFromUtf32(
+                _to
+                , &_toLength
+                , _FROM
+                , _fromLength
+            );
+        }
+    };
+
+    struct String
+    {
+        static const gf::Size   MAX_CHAR_LENGTH;
+    };
+
+    const gf::Size  String::MAX_CHAR_LENGTH = GF_STRING_MAX_CHAR_LENGTH;
+
+    template< typename FROM_T >
     gf::Bool toString(
         gf::String &        _to
         , const FROM_T &    _FROM
-        , const CONVERTER & _CONVERTER
     )
     {
-        return convert(
+        return convert<
+            String::MAX_CHAR_LENGTH
+            , ToString
+        >(
             _to
             , _FROM
-            , _CONVERTER
-            , getMaxStringCharLength()
         );
     }
 
+    template< typename T >
+    struct Unicode
+    {
+        static const gf::Size   MAX_CHAR_LENGTH;
+    };
+
+    template<>
+    const gf::Size  Unicode< gf::Utf8 >::MAX_CHAR_LENGTH = GF_UTF8_MAX_CHAR_LENGTH;
+
+    template<>
+    const gf::Size  Unicode< gf::Utf16 >::MAX_CHAR_LENGTH = GF_UTF16_MAX_CHAR_LENGTH;
+
+    template<>
+    const gf::Size  Unicode< gf::Utf32 >::MAX_CHAR_LENGTH = GF_UTF32_MAX_CHAR_LENGTH;
+
     template<
-        typename TO_T
+        typename CONVERTER
+        , typename TO_T
         , typename FROM_T
-        , typename CONVERTER
     >
     gf::Bool toUnicode(
         TO_T &              _to
         , const FROM_T &    _FROM
-        , const CONVERTER & _CONVERTER
     )
     {
-        return convert(
+        return convert<
+            Unicode< TO_T >::MAX_CHAR_LENGTH
+            , CONVERTER
+        >(
             _to
             , _FROM
-            , _CONVERTER
-            , getMaxCharLength( _to )
+        );
+    }
+
+    struct FromString
+    {
+        gf::Bool operator()(
+            gf::Utf8Char *              _to
+            , gf::Size &                _toLength
+            , const gf::StringChar *    _FROM
+            , gf::Size                  _fromLength
+        )
+        {
+            return gfUtf8FromString(
+                _to
+                , &_toLength
+                , _FROM
+                , _fromLength
+            );
+        }
+
+        gf::Bool operator()(
+            gf::Utf16Char *             _to
+            , gf::Size &                _toLength
+            , const gf::StringChar *    _FROM
+            , gf::Size                  _fromLength
+        )
+        {
+            return gfUtf16FromString(
+                _to
+                , &_toLength
+                , _FROM
+                , _fromLength
+            );
+        }
+
+        gf::Bool operator()(
+            gf::Utf32Char *             _to
+            , gf::Size &                _toLength
+            , const gf::StringChar *    _FROM
+            , gf::Size                  _fromLength
+        )
+        {
+            return gfUtf32FromString(
+                _to
+                , &_toLength
+                , _FROM
+                , _fromLength
+            );
+        }
+    };
+
+    template< typename TO_T >
+    gf::Bool fromString(
+        TO_T &                  _to
+        , const gf::String &    _FROM
+    )
+    {
+        return toUnicode< FromString >(
+            _to
+            , _FROM
+        );
+    }
+
+    struct ConvertUnicode
+    {
+        gf::Bool operator()(
+            gf::Utf8Char *          _to
+            , gf::Size &            _toLength
+            , const gf::Utf16Char * _FROM
+            , gf::Size              _fromLength
+        )
+        {
+            return gfUtf8FromUtf16(
+                _to
+                , &_toLength
+                , _FROM
+                , _fromLength
+            );
+        }
+
+        gf::Bool operator()(
+            gf::Utf8Char *          _to
+            , gf::Size &            _toLength
+            , const gf::Utf32Char * _FROM
+            , gf::Size              _fromLength
+        )
+        {
+            return gfUtf8FromUtf32(
+                _to
+                , &_toLength
+                , _FROM
+                , _fromLength
+            );
+        }
+
+        gf::Bool operator()(
+            gf::Utf16Char *         _to
+            , gf::Size &            _toLength
+            , const gf::Utf8Char *  _FROM
+            , gf::Size              _fromLength
+        )
+        {
+            return gfUtf16FromUtf8(
+                _to
+                , &_toLength
+                , _FROM
+                , _fromLength
+            );
+        }
+
+        gf::Bool operator()(
+            gf::Utf16Char *         _to
+            , gf::Size &            _toLength
+            , const gf::Utf32Char * _FROM
+            , gf::Size              _fromLength
+        )
+        {
+            return gfUtf16FromUtf32(
+                _to
+                , &_toLength
+                , _FROM
+                , _fromLength
+            );
+        }
+
+        gf::Bool operator()(
+            gf::Utf32Char *         _to
+            , gf::Size &            _toLength
+            , const gf::Utf8Char *  _FROM
+            , gf::Size              _fromLength
+        )
+        {
+            return gfUtf32FromUtf8(
+                _to
+                , &_toLength
+                , _FROM
+                , _fromLength
+            );
+        }
+
+        gf::Bool operator()(
+            gf::Utf32Char *         _to
+            , gf::Size &            _toLength
+            , const gf::Utf16Char * _FROM
+            , gf::Size              _fromLength
+        )
+        {
+            return gfUtf32FromUtf16(
+                _to
+                , &_toLength
+                , _FROM
+                , _fromLength
+            );
+        }
+    };
+
+    template<
+        typename TO_T
+        , typename FROM_T
+    >
+    gf::Bool convertUnicode(
+        TO_T &              _to
+        , const FROM_T &    _FROM
+    )
+    {
+        return toUnicode< ConvertUnicode >(
+            _to
+            , _FROM
         );
     }
 }
@@ -111,23 +318,9 @@ namespace gf {
         , const String &    _FROM
     )
     {
-        return toUnicode(
+        return ::fromString(
             _to
             , _FROM
-            , [](
-                Utf8Char *              _to
-                , Size &                _toLength
-                , const StringChar *    _FROM
-                , Size                  _fromLength
-            )
-            {
-                return gfUtf8FromString(
-                    _to
-                    , &_toLength
-                    , _FROM
-                    , _fromLength
-                );
-            }
         );
     }
 
@@ -136,23 +329,9 @@ namespace gf {
         , const String &    _FROM
     )
     {
-        return toUnicode(
+        return ::fromString(
             _to
             , _FROM
-            , [](
-                Utf16Char *             _to
-                , Size &                _toLength
-                , const StringChar *    _FROM
-                , Size                  _fromLength
-            )
-            {
-                return gfUtf16FromString(
-                    _to
-                    , &_toLength
-                    , _FROM
-                    , _fromLength
-                );
-            }
         );
     }
 
@@ -161,23 +340,9 @@ namespace gf {
         , const String &    _FROM
     )
     {
-        return toUnicode(
+        return ::fromString(
             _to
             , _FROM
-            , [](
-                Utf32Char *             _to
-                , Size &                _toLength
-                , const StringChar *    _FROM
-                , Size                  _fromLength
-            )
-            {
-                return gfUtf32FromString(
-                    _to
-                    , &_toLength
-                    , _FROM
-                    , _fromLength
-                );
-            }
         );
     }
 
@@ -189,20 +354,6 @@ namespace gf {
         return ::toString(
             _to
             , _FROM
-            , [](
-                StringChar *        _to
-                , Size &            _toLength
-                , const Utf8Char *  _FROM
-                , Size              _fromLength
-            )
-            {
-                return gfStringFromUtf8(
-                    _to
-                    , &_toLength
-                    , _FROM
-                    , _fromLength
-                );
-            }
         );
     }
 
@@ -214,20 +365,6 @@ namespace gf {
         return ::toString(
             _to
             , _FROM
-            , [](
-                StringChar *        _to
-                , Size &            _toLength
-                , const Utf16Char * _FROM
-                , Size              _fromLength
-            )
-            {
-                return gfStringFromUtf16(
-                    _to
-                    , &_toLength
-                    , _FROM
-                    , _fromLength
-                );
-            }
         );
     }
 
@@ -239,20 +376,6 @@ namespace gf {
         return ::toString(
             _to
             , _FROM
-            , [](
-                StringChar *        _to
-                , Size &            _toLength
-                , const Utf32Char * _FROM
-                , Size              _fromLength
-            )
-            {
-                return gfStringFromUtf32(
-                    _to
-                    , &_toLength
-                    , _FROM
-                    , _fromLength
-                );
-            }
         );
     }
 
@@ -261,23 +384,9 @@ namespace gf {
         , const Utf16 & _FROM
     )
     {
-        return toUnicode(
+        return convertUnicode(
             _to
             , _FROM
-            , [](
-                Utf8Char *          _to
-                , Size &            _toLength
-                , const Utf16Char * _FROM
-                , Size              _fromLength
-            )
-            {
-                return gfUtf8FromUtf16(
-                    _to
-                    , &_toLength
-                    , _FROM
-                    , _fromLength
-                );
-            }
         );
     }
 
@@ -286,23 +395,9 @@ namespace gf {
         , const Utf32 & _FROM
     )
     {
-        return toUnicode(
+        return convertUnicode(
             _to
             , _FROM
-            , [](
-                Utf8Char *          _to
-                , Size &            _toLength
-                , const Utf32Char * _FROM
-                , Size              _fromLength
-            )
-            {
-                return gfUtf8FromUtf32(
-                    _to
-                    , &_toLength
-                    , _FROM
-                    , _fromLength
-                );
-            }
         );
     }
 
@@ -311,23 +406,9 @@ namespace gf {
         , const Utf8 &  _FROM
     )
     {
-        return toUnicode(
+        return convertUnicode(
             _to
             , _FROM
-            , [](
-                Utf16Char *         _to
-                , Size &            _toLength
-                , const Utf8Char *  _FROM
-                , Size              _fromLength
-            )
-            {
-                return gfUtf16FromUtf8(
-                    _to
-                    , &_toLength
-                    , _FROM
-                    , _fromLength
-                );
-            }
         );
     }
 
@@ -336,23 +417,9 @@ namespace gf {
         , const Utf32 & _FROM
     )
     {
-        return toUnicode(
+        return convertUnicode(
             _to
             , _FROM
-            , [](
-                Utf16Char *         _to
-                , Size &            _toLength
-                , const Utf32Char * _FROM
-                , Size              _fromLength
-            )
-            {
-                return gfUtf16FromUtf32(
-                    _to
-                    , &_toLength
-                    , _FROM
-                    , _fromLength
-                );
-            }
         );
     }
 
@@ -361,23 +428,9 @@ namespace gf {
         , const Utf8 &  _FROM
     )
     {
-        return toUnicode(
+        return convertUnicode(
             _to
             , _FROM
-            , [](
-                Utf32Char *         _to
-                , Size &            _toLength
-                , const Utf8Char *  _FROM
-                , Size              _fromLength
-            )
-            {
-                return gfUtf32FromUtf8(
-                    _to
-                    , &_toLength
-                    , _FROM
-                    , _fromLength
-                );
-            }
         );
     }
 
@@ -386,23 +439,9 @@ namespace gf {
         , const Utf16 & _FROM
     )
     {
-        return toUnicode(
+        return convertUnicode(
             _to
             , _FROM
-            , [](
-                Utf32Char *         _to
-                , Size &            _toLength
-                , const Utf16Char * _FROM
-                , Size              _fromLength
-            )
-            {
-                return gfUtf32FromUtf16(
-                    _to
-                    , &_toLength
-                    , _FROM
-                    , _fromLength
-                );
-            }
         );
     }
 }
