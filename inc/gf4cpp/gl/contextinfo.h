ee0f3dced53bc161a5857e4e1dd01c817a47c1d8
tekuto <g.tekuto@gmail.com>
2013-11-26 01:05:45 +0900

    以下のインライン関数を追加 ・gf::toGf() ・gf::toGfpp() ・gf::newGLContextInfo() ・gf::clone() ・gf::free()

---
+++ inc/gf4cpp/gl/contextinfo.h
@@ -0,0 +1,91 @@
+﻿#ifndef GF4CPP_GL_CONTEXTINFO_H
+#define GF4CPP_GL_CONTEXTINFO_H
+
+#include "gfpp/gl/contextinfo.h"
+#include "gf/gl/contextinfo.h"
+
+namespace gf {
+    inline const GfGLContextInfo & toGf(
+        const GLContextInfo &   _INFO
+    )
+    {
+        return reinterpret_cast< const GfGLContextInfo & >( _INFO );
+    }
+
+    inline GfGLContextInfo & toGf(
+        GLContextInfo & _info
+    )
+    {
+        return reinterpret_cast< GfGLContextInfo & >( _info );
+    }
+
+    inline const GfGLContextInfo * toGf(
+        const GLContextInfo *   _INFO
+    )
+    {
+        return reinterpret_cast< const GfGLContextInfo * >( _INFO );
+    }
+
+    inline GfGLContextInfo * toGf(
+        GLContextInfo * _info
+    )
+    {
+        return reinterpret_cast< GfGLContextInfo * >( _info );
+    }
+
+    inline const GLContextInfo & toGfpp(
+        const GfGLContextInfo & _INFO
+    )
+    {
+        return reinterpret_cast< const GLContextInfo & >( _INFO );
+    }
+
+    inline GLContextInfo & toGfpp(
+        GfGLContextInfo &   _info
+    )
+    {
+        return reinterpret_cast< GLContextInfo & >( _info );
+    }
+
+    inline const GLContextInfo * toGfpp(
+        const GfGLContextInfo * _INFO
+    )
+    {
+        return reinterpret_cast< const GLContextInfo * >( _INFO );
+    }
+
+    inline GLContextInfo * toGfpp(
+        GfGLContextInfo *   _info
+    )
+    {
+        return reinterpret_cast< GLContextInfo * >( _info );
+    }
+}
+
+namespace gf {
+    inline GLContextInfo * newGLContextInfo(
+    )
+    {
+        return toGfpp( gfGLContextInfoNew() );
+    }
+
+    inline GLContextInfo * clone(
+        const GLContextInfo &   _INFO
+    )
+    {
+        return toGfpp(
+            gfGLContextInfoClone(
+                toGf( &_INFO )
+            )
+        );
+    }
+
+    inline void free(
+        GLContextInfo & _info
+    )
+    {
+        gfGLContextInfoFree( toGf( &_info ) );
+    }
+}
+
+#endif  // GF4CPP_GL_CONTEXTINFO_H
