adc329faa61c3acfebc2f89a47b8b2c9654d8e2b
tekuto <g.tekuto@gmail.com>
2013-11-30 15:40:42 +0900

    以下のインライン関数を追加 ・fg::toFg() ・fg::toFgpp() ・fg::getGLConfig() ・gf::toGf() ・gf::toGfpp() ・gf::newArgs() ・gf::free()

---
+++ inc/fg4cpp/main/args.h
@@ -0,0 +1,52 @@
+﻿#ifndef FG4CPP_MAIN_ARGS_H
+#define FG4CPP_MAIN_ARGS_H
+
+#include "fgpp/main/args.h"
+#include "fg/main/args.h"
+
+namespace fg {
+    inline const FgArgs & toFg(
+        const Args &    _GAME_CONFIG
+    )
+    {
+        return reinterpret_cast< const FgArgs & >( _GAME_CONFIG );
+    }
+
+    inline const FgArgs * toFg(
+        const Args *    _GAME_CONFIG
+    )
+    {
+        return reinterpret_cast< const FgArgs * >( _GAME_CONFIG );
+    }
+
+    inline const Args & toFgpp(
+        const FgArgs &  _GAME_CONFIG
+    )
+    {
+        return reinterpret_cast< const Args & >( _GAME_CONFIG );
+    }
+
+    inline const Args * toFgpp(
+        const FgArgs *  _GAME_CONFIG
+    )
+    {
+        return reinterpret_cast< const Args * >( _GAME_CONFIG );
+    }
+}
+
+#include "fg4cpp/gl/config.h"
+
+namespace fg {
+    inline const GLConfig & getGLConfig(
+        const Args &    _ARGS
+    )
+    {
+        return toFgpp(
+            *fgArgsGetGLConfig(
+                toFg( &_ARGS )
+            )
+        );
+    }
+}
+
+#endif  // FG4CPP_MAIN_ARGS_H
---
+++ inc/gf4cpp/main/args.h
@@ -0,0 +1,61 @@
+﻿#ifndef GF4CPP_MAIN_ARGS_H
+#define GF4CPP_MAIN_ARGS_H
+
+#include "gfpp/main/args.h"
+#include "gf/main/args.h"
+
+namespace gf {
+    inline FgArgs & toGf(
+        fg::Args &  _gameConfig
+    )
+    {
+        return reinterpret_cast< FgArgs & >( _gameConfig );
+    }
+
+    inline FgArgs * toGf(
+        fg::Args *  _gameConfig
+    )
+    {
+        return reinterpret_cast< FgArgs * >( _gameConfig );
+    }
+
+    inline fg::Args & toGfpp(
+        FgArgs &    _gameConfig
+    )
+    {
+        return reinterpret_cast< fg::Args & >( _gameConfig );
+    }
+
+    inline fg::Args * toGfpp(
+        FgArgs *    _gameConfig
+    )
+    {
+        return reinterpret_cast< fg::Args * >( _gameConfig );
+    }
+}
+
+#include "fg4cpp/boot/gameconfig.h"
+
+namespace gf {
+    inline fg::Args * newArgs(
+        const fg::GameConfig &  _CONFIG
+    )
+    {
+        return toGfpp(
+            gfArgsNew(
+                fg::toFg( &_CONFIG )
+            )
+        );
+    }
+
+    inline void free(
+        fg::Args &  _args
+    )
+    {
+        gfArgsFree(
+            toGf( &_args )
+        );
+    }
+}
+
+#endif  // GF4CPP_MAIN_ARGS_H
