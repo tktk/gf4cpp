3d6ae8e3dc75224a4cbaa915a004190ecce226dd
tekuto <g.tekuto@gmail.com>
2013-12-02 18:11:37 +0900

    以下のインライン関数を追加 ・gf::existsGLDoubleBuffer() ・gf::getGLDoubleBuffer() ・gf::setGLDoubleBuffer() ・gf::clearGLDoubleBuffer() ・gf::existsGLAuxBuffers() ・gf::getGLAuxBuffers() ・gf::setGLAuxBuffers() ・gf::clearGLAuxBuffers() ・gf::existsGLRedBits() ・gf::getGLRedBits() ・gf::setGLRedBits() ・gf::clearGLRedBits() ・gf::existsGLGreenBits() ・gf::getGLGreenBits() ・gf::setGLGreenBits() ・gf::clearGLGreenBits() ・gf::existsGLBlueBits() ・gf::getGLBlueBits() ・gf::setGLBlueBits() ・gf::clearGLBlueBits() ・gf::existsGLAlphaBits() ・gf::getGLAlphaBits() ・gf::setGLAlphaBits() ・gf::clearGLAlphaBits() ・gf::existsGLDepthBits() ・gf::getGLDepthBits() ・gf::setGLDepthBits() ・gf::clearGLDepthBits() ・gf::existsGLStencilBits() ・gf::getGLStencilBits() ・gf::setGLStencilBits() ・gf::clearGLStencilBits() ・gf::existsGLAccumRedBits() ・gf::getGLAccumRedBits() ・gf::setGLAccumRedBits() ・gf::clearGLAccumRedBits() ・gf::existsGLAccumGreenBits() ・gf::getGLAccumGreenBits() ・gf::setGLAccumGreenBits() ・gf::clearGLAccumGreenBits() ・gf::existsGLAccumBlueBits() ・gf::getGLAccumBlueBits() ・gf::setGLAccumBlueBits() ・gf::clearGLAccumBlueBits() ・gf::existsGLAccumAlphaBits() ・gf::getGLAccumAlphaBits() ・gf::setGLAccumAlphaBits() ・gf::clearGLAccumAlphaBits()

--- inc/gf4cpp/boot/config.h
+++ inc/gf4cpp/boot/config.h
@@ -120,6 +120,462 @@ namespace gf {
             toGf( &_config )
         );
     }
+
+    inline fg::Bool existsGLDoubleBuffer(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigExistsGLDoubleBuffer(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline fg::Bool getGLDoubleBuffer(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigGetGLDoubleBuffer(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline void setGLDoubleBuffer(
+        BootConfig &    _config
+        , fg::Bool      _doubleBuffer
+    )
+    {
+        gfBootConfigSetGLDoubleBuffer(
+            toGf( &_config )
+            , _doubleBuffer
+        );
+    }
+
+    inline void clearGLDoubleBuffer(
+        BootConfig &    _config
+    )
+    {
+        gfBootConfigClearGLDoubleBuffer(
+            toGf( &_config )
+        );
+    }
+
+    inline fg::Bool existsGLAuxBuffers(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigExistsGLAuxBuffers(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline fg::Int getGLAuxBuffers(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigGetGLAuxBuffers(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline void setGLAuxBuffers(
+        BootConfig &    _config
+        , fg::Int       _auxBuffers
+    )
+    {
+        gfBootConfigSetGLAuxBuffers(
+            toGf( &_config )
+            , _auxBuffers
+        );
+    }
+
+    inline void clearGLAuxBuffers(
+        BootConfig &    _config
+    )
+    {
+        gfBootConfigClearGLAuxBuffers(
+            toGf( &_config )
+        );
+    }
+
+    inline fg::Bool existsGLRedBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigExistsGLRedBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline fg::Int getGLRedBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigGetGLRedBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline void setGLRedBits(
+        BootConfig &    _config
+        , fg::Int       _redBits
+    )
+    {
+        gfBootConfigSetGLRedBits(
+            toGf( &_config )
+            , _redBits
+        );
+    }
+
+    inline void clearGLRedBits(
+        BootConfig &    _config
+    )
+    {
+        gfBootConfigClearGLRedBits(
+            toGf( &_config )
+        );
+    }
+
+    inline fg::Bool existsGLGreenBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigExistsGLGreenBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline fg::Int getGLGreenBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigGetGLGreenBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline void setGLGreenBits(
+        BootConfig &    _config
+        , fg::Int       _greenBits
+    )
+    {
+        gfBootConfigSetGLGreenBits(
+            toGf( &_config )
+            , _greenBits
+        );
+    }
+
+    inline void clearGLGreenBits(
+        BootConfig &    _config
+    )
+    {
+        gfBootConfigClearGLGreenBits(
+            toGf( &_config )
+        );
+    }
+
+    inline fg::Bool existsGLBlueBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigExistsGLBlueBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline fg::Int getGLBlueBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigGetGLBlueBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline void setGLBlueBits(
+        BootConfig &    _config
+        , fg::Int       _blueBits
+    )
+    {
+        gfBootConfigSetGLBlueBits(
+            toGf( &_config )
+            , _blueBits
+        );
+    }
+
+    inline void clearGLBlueBits(
+        BootConfig &    _config
+    )
+    {
+        gfBootConfigClearGLBlueBits(
+            toGf( &_config )
+        );
+    }
+
+    inline fg::Bool existsGLAlphaBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigExistsGLAlphaBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline fg::Int getGLAlphaBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigGetGLAlphaBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline void setGLAlphaBits(
+        BootConfig &    _config
+        , fg::Int       _alphaBits
+    )
+    {
+        gfBootConfigSetGLAlphaBits(
+            toGf( &_config )
+            , _alphaBits
+        );
+    }
+
+    inline void clearGLAlphaBits(
+        BootConfig &    _config
+    )
+    {
+        gfBootConfigClearGLAlphaBits(
+            toGf( &_config )
+        );
+    }
+
+    inline fg::Bool existsGLDepthBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigExistsGLDepthBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline fg::Int getGLDepthBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigGetGLDepthBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline void setGLDepthBits(
+        BootConfig &    _config
+        , fg::Int       _depthBits
+    )
+    {
+        gfBootConfigSetGLDepthBits(
+            toGf( &_config )
+            , _depthBits
+        );
+    }
+
+    inline void clearGLDepthBits(
+        BootConfig &    _config
+    )
+    {
+        gfBootConfigClearGLDepthBits(
+            toGf( &_config )
+        );
+    }
+
+    inline fg::Bool existsGLStencilBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigExistsGLStencilBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline fg::Int getGLStencilBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigGetGLStencilBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline void setGLStencilBits(
+        BootConfig &    _config
+        , fg::Int       _stencilBits
+    )
+    {
+        gfBootConfigSetGLStencilBits(
+            toGf( &_config )
+            , _stencilBits
+        );
+    }
+
+    inline void clearGLStencilBits(
+        BootConfig &    _config
+    )
+    {
+        gfBootConfigClearGLStencilBits(
+            toGf( &_config )
+        );
+    }
+
+    inline fg::Bool existsGLAccumRedBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigExistsGLAccumRedBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline fg::Int getGLAccumRedBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigGetGLAccumRedBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline void setGLAccumRedBits(
+        BootConfig &    _config
+        , fg::Int       _accumRedBits
+    )
+    {
+        gfBootConfigSetGLAccumRedBits(
+            toGf( &_config )
+            , _accumRedBits
+        );
+    }
+
+    inline void clearGLAccumRedBits(
+        BootConfig &    _config
+    )
+    {
+        gfBootConfigClearGLAccumRedBits(
+            toGf( &_config )
+        );
+    }
+
+    inline fg::Bool existsGLAccumGreenBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigExistsGLAccumGreenBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline fg::Int getGLAccumGreenBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigGetGLAccumGreenBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline void setGLAccumGreenBits(
+        BootConfig &    _config
+        , fg::Int       _accumGreenBits
+    )
+    {
+        gfBootConfigSetGLAccumGreenBits(
+            toGf( &_config )
+            , _accumGreenBits
+        );
+    }
+
+    inline void clearGLAccumGreenBits(
+        BootConfig &    _config
+    )
+    {
+        gfBootConfigClearGLAccumGreenBits(
+            toGf( &_config )
+        );
+    }
+
+    inline fg::Bool existsGLAccumBlueBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigExistsGLAccumBlueBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline fg::Int getGLAccumBlueBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigGetGLAccumBlueBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline void setGLAccumBlueBits(
+        BootConfig &    _config
+        , fg::Int       _accumBlueBits
+    )
+    {
+        gfBootConfigSetGLAccumBlueBits(
+            toGf( &_config )
+            , _accumBlueBits
+        );
+    }
+
+    inline void clearGLAccumBlueBits(
+        BootConfig &    _config
+    )
+    {
+        gfBootConfigClearGLAccumBlueBits(
+            toGf( &_config )
+        );
+    }
+
+    inline fg::Bool existsGLAccumAlphaBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigExistsGLAccumAlphaBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline fg::Int getGLAccumAlphaBits(
+        const BootConfig &  _CONFIG
+    )
+    {
+        return gfBootConfigGetGLAccumAlphaBits(
+            toGf( &_CONFIG )
+        );
+    }
+
+    inline void setGLAccumAlphaBits(
+        BootConfig &    _config
+        , fg::Int       _accumAlphaBits
+    )
+    {
+        gfBootConfigSetGLAccumAlphaBits(
+            toGf( &_config )
+            , _accumAlphaBits
+        );
+    }
+
+    inline void clearGLAccumAlphaBits(
+        BootConfig &    _config
+    )
+    {
+        gfBootConfigClearGLAccumAlphaBits(
+            toGf( &_config )
+        );
+    }
 }
 
 #endif  // GF4CPP_BOOT_CONFIG_H
