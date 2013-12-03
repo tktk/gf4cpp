13c7238c794f304d2515aac99668dcbc09fcec83
tekuto <g.tekuto@gmail.com>
2013-12-03 17:01:09 +0900

    以下のインライン関数を追加 ・fg::glSwapBuffers()

---
+++ inc/fg4cpp/gl/gl.h
@@ -0,0 +1,19 @@
+﻿#ifndef FG4CPP_GL_GL_H
+#define FG4CPP_GL_GL_H
+
+#include "fgpp/gl/gl.h"
+#include "fg/gl/gl.h"
+#include "fg4cpp/window/window.h"
+
+namespace fg {
+    inline void glSwapBuffers(
+        Window &    _window
+    )
+    {
+        fgGLSwapBuffers(
+            toFg( &_window )
+        );
+    }
+}
+
+#endif  // FG4CPP_GL_GL_H
