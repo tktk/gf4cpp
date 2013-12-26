ee27a94f05b72f994e52e4d6973d9c4972381eba
tekuto <g.tekuto@gmail.com>
2013-12-26 20:35:39 +0900

    fgのヘッダファイル分割、移動に伴う修正を適用

--- inc/gf4cpp/main/boot.h
+++ inc/gf4cpp/main/boot.h
@@ -1,11 +1,11 @@
 ﻿#ifndef GF4CPP_MAIN_BOOT_H
 #define GF4CPP_MAIN_BOOT_H
 
+#include "gfpp/main/boot.h"
 #include "gf/main/boot.h"
-
-#include "gf4cpp/main/bootconfig.h"
-#include "fg4cpp/string/utf32.h"
-#include "fgpp/common/primitives.h"
+#include "gf4cpp/def/main/bootconfig.h"
+#include "fg4cpp/def/string/utf32.h"
+#include "fgpp/def/common/primitives.h"
 
 namespace gf {
     inline fg::Int boot(
