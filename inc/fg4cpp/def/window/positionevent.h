a34b0c59045b64a9f71d177fde4f4a1c5fcbfe7b
tekuto <g.tekuto@gmail.com>
2013-12-26 19:30:58 +0900

    fg::WindowPositionEventに関係する以下の関数を別のファイルに分離 ・fg::toFg() ・fg::toFgpp() fgのヘッダファイル分割、移動に伴う修正を適用

---
+++ inc/fg4cpp/def/window/positionevent.h
@@ -0,0 +1,65 @@
+﻿#ifndef FG4CPP_DEF_WINDOW_POSITIONEVENT_H
+#define FG4CPP_DEF_WINDOW_POSITIONEVENT_H
+
+#include "fgpp/def/window/positionevent.h"
+#include "fg/def/window/positionevent.h"
+
+namespace fg {
+    inline const FgWindowPositionEvent & toFg(
+        const WindowPositionEvent & _EVENT
+    )
+    {
+        return reinterpret_cast< const FgWindowPositionEvent & >( _EVENT );
+    }
+
+    inline FgWindowPositionEvent & toFg(
+        WindowPositionEvent &   _event
+    )
+    {
+        return reinterpret_cast< FgWindowPositionEvent & >( _event );
+    }
+
+    inline const FgWindowPositionEvent * toFg(
+        const WindowPositionEvent * _EVENT
+    )
+    {
+        return reinterpret_cast< const FgWindowPositionEvent * >( _EVENT );
+    }
+
+    inline FgWindowPositionEvent * toFg(
+        WindowPositionEvent *   _event
+    )
+    {
+        return reinterpret_cast< FgWindowPositionEvent * >( _event );
+    }
+
+    inline const WindowPositionEvent & toFgpp(
+        const FgWindowPositionEvent &   _EVENT
+    )
+    {
+        return reinterpret_cast< const WindowPositionEvent & >( _EVENT );
+    }
+
+    inline WindowPositionEvent & toFgpp(
+        FgWindowPositionEvent & _event
+    )
+    {
+        return reinterpret_cast< WindowPositionEvent & >( _event );
+    }
+
+    inline const WindowPositionEvent * toFgpp(
+        const FgWindowPositionEvent *   _EVENT
+    )
+    {
+        return reinterpret_cast< const WindowPositionEvent * >( _EVENT );
+    }
+
+    inline WindowPositionEvent * toFgpp(
+        FgWindowPositionEvent * _event
+    )
+    {
+        return reinterpret_cast< WindowPositionEvent * >( _event );
+    }
+}
+
+#endif  // FG4CPP_DEF_WINDOW_POSITIONEVENT_H
--- inc/fg4cpp/window/positionevent.h
+++ inc/fg4cpp/window/positionevent.h
@@ -3,67 +3,9 @@
 
 #include "fgpp/window/positionevent.h"
 #include "fg/window/positionevent.h"
-
-namespace fg {
-    inline const FgWindowPositionEvent & toFg(
-        const WindowPositionEvent & _EVENT
-    )
-    {
-        return reinterpret_cast< const FgWindowPositionEvent & >( _EVENT );
-    }
-
-    inline FgWindowPositionEvent & toFg(
-        WindowPositionEvent &   _event
-    )
-    {
-        return reinterpret_cast< FgWindowPositionEvent & >( _event );
-    }
-
-    inline const FgWindowPositionEvent * toFg(
-        const WindowPositionEvent * _EVENT
-    )
-    {
-        return reinterpret_cast< const FgWindowPositionEvent * >( _EVENT );
-    }
-
-    inline FgWindowPositionEvent * toFg(
-        WindowPositionEvent *   _event
-    )
-    {
-        return reinterpret_cast< FgWindowPositionEvent * >( _event );
-    }
-
-    inline const WindowPositionEvent & toFgpp(
-        const FgWindowPositionEvent &   _EVENT
-    )
-    {
-        return reinterpret_cast< const WindowPositionEvent & >( _EVENT );
-    }
-
-    inline WindowPositionEvent & toFgpp(
-        FgWindowPositionEvent & _event
-    )
-    {
-        return reinterpret_cast< WindowPositionEvent & >( _event );
-    }
-
-    inline const WindowPositionEvent * toFgpp(
-        const FgWindowPositionEvent *   _EVENT
-    )
-    {
-        return reinterpret_cast< const WindowPositionEvent * >( _EVENT );
-    }
-
-    inline WindowPositionEvent * toFgpp(
-        FgWindowPositionEvent * _event
-    )
-    {
-        return reinterpret_cast< WindowPositionEvent * >( _event );
-    }
-}
-
-#include "fg4cpp/window/window.h"
-#include "fgpp/common/primitives.h"
+#include "fg4cpp/def/window/positionevent.h"
+#include "fg4cpp/def/window/window.h"
+#include "fgpp/def/common/primitives.h"
 
 namespace fg {
     inline WindowPositionEvent * newWindowPositionEvent(
