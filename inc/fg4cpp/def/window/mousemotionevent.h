4b1fee799e604e197b1178f28e1cec8d5d38a9cc
tekuto <g.tekuto@gmail.com>
2013-12-26 19:41:45 +0900

    fg::WindowMouseMotionEventに関係する以下の関数を別のファイルに分離 ・fg::toFg() ・fg::toFgpp() fgのヘッダファイル分割、移動に伴う修正を適用

---
+++ inc/fg4cpp/def/window/mousemotionevent.h
@@ -0,0 +1,65 @@
+﻿#ifndef FG4CPP_DEF_WINDOW_MOUSEMOTIONEVENT_H
+#define FG4CPP_DEF_WINDOW_MOUSEMOTIONEVENT_H
+
+#include "fgpp/def/window/mousemotionevent.h"
+#include "fg/def/window/mousemotionevent.h"
+
+namespace fg {
+    inline const FgWindowMouseMotionEvent & toFg(
+        const WindowMouseMotionEvent &  _EVENT
+    )
+    {
+        return reinterpret_cast< const FgWindowMouseMotionEvent & >( _EVENT );
+    }
+
+    inline FgWindowMouseMotionEvent & toFg(
+        WindowMouseMotionEvent &    _event
+    )
+    {
+        return reinterpret_cast< FgWindowMouseMotionEvent & >( _event );
+    }
+
+    inline const FgWindowMouseMotionEvent * toFg(
+        const WindowMouseMotionEvent *  _EVENT
+    )
+    {
+        return reinterpret_cast< const FgWindowMouseMotionEvent * >( _EVENT );
+    }
+
+    inline FgWindowMouseMotionEvent * toFg(
+        WindowMouseMotionEvent *    _event
+    )
+    {
+        return reinterpret_cast< FgWindowMouseMotionEvent * >( _event );
+    }
+
+    inline const WindowMouseMotionEvent & toFgpp(
+        const FgWindowMouseMotionEvent &    _EVENT
+    )
+    {
+        return reinterpret_cast< const WindowMouseMotionEvent & >( _EVENT );
+    }
+
+    inline WindowMouseMotionEvent & toFgpp(
+        FgWindowMouseMotionEvent &  _event
+    )
+    {
+        return reinterpret_cast< WindowMouseMotionEvent & >( _event );
+    }
+
+    inline const WindowMouseMotionEvent * toFgpp(
+        const FgWindowMouseMotionEvent *    _EVENT
+    )
+    {
+        return reinterpret_cast< const WindowMouseMotionEvent * >( _EVENT );
+    }
+
+    inline WindowMouseMotionEvent * toFgpp(
+        FgWindowMouseMotionEvent *  _event
+    )
+    {
+        return reinterpret_cast< WindowMouseMotionEvent * >( _event );
+    }
+}
+
+#endif  // FG4CPP_DEF_WINDOW_MOUSEMOTIONEVENT_H
--- inc/fg4cpp/window/mousemotionevent.h
+++ inc/fg4cpp/window/mousemotionevent.h
@@ -3,67 +3,9 @@
 
 #include "fgpp/window/mousemotionevent.h"
 #include "fg/window/mousemotionevent.h"
-
-namespace fg {
-    inline const FgWindowMouseMotionEvent & toFg(
-        const WindowMouseMotionEvent &  _EVENT
-    )
-    {
-        return reinterpret_cast< const FgWindowMouseMotionEvent & >( _EVENT );
-    }
-
-    inline FgWindowMouseMotionEvent & toFg(
-        WindowMouseMotionEvent &    _event
-    )
-    {
-        return reinterpret_cast< FgWindowMouseMotionEvent & >( _event );
-    }
-
-    inline const FgWindowMouseMotionEvent * toFg(
-        const WindowMouseMotionEvent *  _EVENT
-    )
-    {
-        return reinterpret_cast< const FgWindowMouseMotionEvent * >( _EVENT );
-    }
-
-    inline FgWindowMouseMotionEvent * toFg(
-        WindowMouseMotionEvent *    _event
-    )
-    {
-        return reinterpret_cast< FgWindowMouseMotionEvent * >( _event );
-    }
-
-    inline const WindowMouseMotionEvent & toFgpp(
-        const FgWindowMouseMotionEvent &    _EVENT
-    )
-    {
-        return reinterpret_cast< const WindowMouseMotionEvent & >( _EVENT );
-    }
-
-    inline WindowMouseMotionEvent & toFgpp(
-        FgWindowMouseMotionEvent &  _event
-    )
-    {
-        return reinterpret_cast< WindowMouseMotionEvent & >( _event );
-    }
-
-    inline const WindowMouseMotionEvent * toFgpp(
-        const FgWindowMouseMotionEvent *    _EVENT
-    )
-    {
-        return reinterpret_cast< const WindowMouseMotionEvent * >( _EVENT );
-    }
-
-    inline WindowMouseMotionEvent * toFgpp(
-        FgWindowMouseMotionEvent *  _event
-    )
-    {
-        return reinterpret_cast< WindowMouseMotionEvent * >( _event );
-    }
-}
-
-#include "fg4cpp/window/window.h"
-#include "fgpp/common/primitives.h"
+#include "fg4cpp/def/window/mousemotionevent.h"
+#include "fg4cpp/def/window/window.h"
+#include "fgpp/def/common/primitives.h"
 
 namespace fg {
     inline WindowMouseMotionEvent * newWindowMouseMotionEvent(
