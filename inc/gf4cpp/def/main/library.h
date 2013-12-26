22606d1cb8e2d427c6d8a501970e3b981a44cac1
tekuto <g.tekuto@gmail.com>
2013-12-26 20:29:54 +0900

    gf::Libraryに関係する以下の関数を別のファイルに分離 ・gf::toGf() ・g:f:toGfpp() fgのヘッダファイル分割、移動に伴う修正を適用

---
+++ inc/gf4cpp/def/main/library.h
@@ -0,0 +1,65 @@
+﻿#ifndef GF4CPP_DEF_MAIN_LIBRARY_H
+#define GF4CPP_DEF_MAIN_LIBRARY_H
+
+#include "gfpp/def/main/library.h"
+#include "gf/def/main/library.h"
+
+namespace gf {
+    inline const GfLibrary & toGf(
+        const Library & _LIBRARY
+    )
+    {
+        return reinterpret_cast< const GfLibrary & >( _LIBRARY );
+    }
+
+    inline GfLibrary & toGf(
+        Library &   _library
+    )
+    {
+        return reinterpret_cast< GfLibrary & >( _library );
+    }
+
+    inline const GfLibrary * toGf(
+        const Library * _LIBRARY
+    )
+    {
+        return reinterpret_cast< const GfLibrary * >( _LIBRARY );
+    }
+
+    inline GfLibrary * toGf(
+        Library *   _library
+    )
+    {
+        return reinterpret_cast< GfLibrary * >( _library );
+    }
+
+    inline const Library & toGfpp(
+        const GfLibrary &   _LIBRARY
+    )
+    {
+        return reinterpret_cast< const Library & >( _LIBRARY );
+    }
+
+    inline Library & toGfpp(
+        GfLibrary & _library
+    )
+    {
+        return reinterpret_cast< Library & >( _library );
+    }
+
+    inline const Library * toGfpp(
+        const GfLibrary *   _LIBRARY
+    )
+    {
+        return reinterpret_cast< const Library * >( _LIBRARY );
+    }
+
+    inline Library * toGfpp(
+        GfLibrary * _library
+    )
+    {
+        return reinterpret_cast< Library * >( _library );
+    }
+}
+
+#endif  // GF4CPP_DEF_MAIN_LIBRARY_H
--- inc/gf4cpp/main/library.h
+++ inc/gf4cpp/main/library.h
@@ -3,66 +3,8 @@
 
 #include "gfpp/main/library.h"
 #include "gf/main/library.h"
-
-namespace gf {
-    inline const GfLibrary & toGf(
-        const Library & _LIBRARY
-    )
-    {
-        return reinterpret_cast< const GfLibrary & >( _LIBRARY );
-    }
-
-    inline GfLibrary & toGf(
-        Library &   _library
-    )
-    {
-        return reinterpret_cast< GfLibrary & >( _library );
-    }
-
-    inline const GfLibrary * toGf(
-        const Library * _LIBRARY
-    )
-    {
-        return reinterpret_cast< const GfLibrary * >( _LIBRARY );
-    }
-
-    inline GfLibrary * toGf(
-        Library *   _library
-    )
-    {
-        return reinterpret_cast< GfLibrary * >( _library );
-    }
-
-    inline const Library & toGfpp(
-        const GfLibrary &   _LIBRARY
-    )
-    {
-        return reinterpret_cast< const Library & >( _LIBRARY );
-    }
-
-    inline Library & toGfpp(
-        GfLibrary & _library
-    )
-    {
-        return reinterpret_cast< Library & >( _library );
-    }
-
-    inline const Library * toGfpp(
-        const GfLibrary *   _LIBRARY
-    )
-    {
-        return reinterpret_cast< const Library * >( _LIBRARY );
-    }
-
-    inline Library * toGfpp(
-        GfLibrary * _library
-    )
-    {
-        return reinterpret_cast< Library * >( _library );
-    }
-}
-
-#include "fg4cpp/string/utf32.h"
+#include "gf4cpp/def/main/library.h"
+#include "fg4cpp/def/string/utf32.h"
 
 namespace gf {
     inline Library * newLibrary(
